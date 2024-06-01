c5410eb1e10f0cbc954364434b780f4c8aabe953
Dheeraj Verma <164219473+DheerajVerma2017@users.noreply.github.com>
2024-06-01 17:13:23 +0530

    Add files via upload

---
+++ FunctionOverloading.java
@@ -0,0 +1,29 @@
+class FunctionOverloading
+{
+ static void show()
+ {
+ System.out.println("Zero Arguments :");
+ }
+ static void show( int x)
+ {
+ System.out.println("One Arguments :");
+ }
+ static void show(int x ,int y)
+ {
+ System.out.println("Two Arguments :");
+ }
+ static void show(int x ,int y ,int z)
+ {
+ System.out.println("Three Arguments :");
+ }
+ public static void main(String[] args)
+ {
+ show();
+ show(10);
+ show(10,20);
+ show(10,20,30);
+ }
+}
+ 
+ 
+ 
